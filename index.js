c24d0c557b08693e405414b3bac984971c7e1712
evoclean <evocleanweb@gmail.com>
2016-10-02 21:03:24 -0600

    Change a lot on the website

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-$("document").ready(mySetup);
-function mySetup {
-	$(".title").css("color", "blue");
-}
+$("#evoLogo").mouseenter(function (){
+	// $("#dropdown").css("-webkit-animation-name", "dropDown");
+	$("#dropdown").css("height", "auto");
+	$("#evoLogo").css("background-image", "url(evoLogo.png)");
+});
+$("#dropdown").mouseleave(function (){
+	// $("#dropdown").css("webkit-animation-name", "");
+	$("#dropdown").css("height", "");
+	$("#evoLogo").css("background-image", "");
+});
+
+$("#dropdownHome").mouseenter(function (){
+	$("#dropdownHome").css("background-color", "#191919");
+	$("#dropdownHome").css("color", "#A9A9AB !important");
+});
+$("#dropdownHome").mouseleave(function (){
+	$("#dropdownHome").css("background-color", "");
+});
+
+$("#dropdownContact").mouseenter(function (){
+	$("#dropdownContact").css("background-color", "#191919");
+});
+$("#dropdownContact").mouseleave(function (){
+	$("#dropdownContact").css("background-color", "");
+});
+
+$("#dropdownAbout").mouseenter(function (){
+	$("#dropdownAbout").css("background-color", "#191919");
+});
+$("#dropdownAbout").mouseleave(function (){
+	$("#dropdownAbout").css("background-color", "");
+});
